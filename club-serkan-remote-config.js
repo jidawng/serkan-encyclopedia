1e06da850a82fc50c28ebfc16bcc04b4d1a3a70b
jidawng <lksjdwgpt@gmail.com>
2026-06-20 18:26:26 +0900

    Enable Club Serkan Supabase sync

--- club-serkan-remote-config.js
+++ club-serkan-remote-config.js
@@ -1,6 +1,6 @@
 window.CLUB_SERKAN_REMOTE = {
-  enabled: false,
-  url: "",
-  anonKey: "",
+  enabled: true,
+  url: "https://qjadteffijrvouhfdogu.supabase.co",
+  anonKey: "sb_publishable_yeDkMQQk3rPUxd1KXnQjhA_tH9n5k5K",
   table: "club_serkan_state",
 };
